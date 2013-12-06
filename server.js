6d71c7fa1a78f5ecf3ad3f2b46f705e0d48e19cb
rupamkhaitan <rupamkhaitan@gmail.com>
2013-12-06 15:23:01 +0530

    updated

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ var url = require('url');
 	response.end();
 }).listen(8888);*/
 
-function start(route){
+function start(route,handle){
 	function onRequest(request, response) {
 		var pathname = url.parse(request.url).pathname;
 		console.log("Request for " + pathname + " received.");
-		route(pathname);
+		route(handle,pathname);
 		response.writeHead(200, {"Content-Type": "text/plain"});
 		response.write("Hello World");
 		response.end();
