f874877e9905029ae83f232e14a50bc0e66f0ca6
rupamkhaitan <rupamkhaitan@gmail.com>
2013-12-06 17:07:37 +0530

    updated with proper code with NON-blocking approach

--- server.js
+++ server.js
@@ -10,10 +10,7 @@ function start(route, handle){
 	function onRequest(request, response) {
 		var pathname = url.parse(request.url).pathname;
 		console.log("Request for " + pathname + " received.");
-		response.writeHead(200, {"Content-Type": "text/plain"});
-		var content1 = route(handle,pathname);
-		response.write(content1);
-		response.end(content);
+		route(handle, pathname, response);
 	}
 	http.createServer(onRequest).listen(8888);
 	console.log("Server has started.");
