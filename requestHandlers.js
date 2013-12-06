01911cc7ee0440a5e4e663c246800261e09d06f9
rupamkhaitan <rupamkhaitan@gmail.com>
2013-12-06 17:10:24 +0530

    with delay

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,7 +1,9 @@
 var exec = require("child_process").exec;
 function start(response) {
 	console.log("Request handler 'start' was called.");
-	exec("ls -lah", function (error, stdout, stderr) {
+	exec("find /",
+		{ timeout: 10000, maxBuffer: 20000*1024 },
+		function (error, stdout, stderr) {
 		response.writeHead(200, {"Content-Type": "text/plain"});
 		response.write(stdout);
 		response.end();
