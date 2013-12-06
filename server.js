d798912b8915bc70dfb72e8f3177e94e687d52d5
rupamkhaitan <rupamkhaitan@gmail.com>
2013-12-06 16:56:53 +0530

    update

--- server.js
+++ server.js
@@ -6,14 +6,14 @@ var url = require('url');
 	response.end();
 }).listen(8888);*/
 
-function start(route,handle){
+function start(route, handle){
 	function onRequest(request, response) {
 		var pathname = url.parse(request.url).pathname;
 		console.log("Request for " + pathname + " received.");
-		route(handle,pathname);
 		response.writeHead(200, {"Content-Type": "text/plain"});
-		response.write("Hello World");
-		response.end();
+		var content = route(handle,pathname)
+		//response.write('',content);
+		response.end(content);
 	}
 	http.createServer(onRequest).listen(8888);
 	console.log("Server has started.");
